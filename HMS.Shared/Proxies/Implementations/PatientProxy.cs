21125e53a2d5c92ca2efc79d865f67650885d7a0
Barnut Cristiana <119601150+criss05@users.noreply.github.com>
2025-06-06 01:41:12 +0300

    modify the user proxy to not receive the token in constructer, but in functions

--- HMS.Shared/Proxies/Implementations/PatientProxy.cs
+++ HMS.Shared/Proxies/Implementations/PatientProxy.cs
@@ -16,17 +16,16 @@ namespace HMS.Shared.Proxies.Implementations
     {
         private readonly HttpClient _http_client;
         private readonly string s_base_api_url = Config._base_api_url;
-        private readonly string _token;
+        private string Token { get; set; }
 
-        public PatientProxy(HttpClient httpClient, string token)
+        public PatientProxy(HttpClient httpClient)
         {
             this._http_client = httpClient;
-            this._token = token;
         }
 
         private void AddAuthorizationHeader()
         {
-            this._http_client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", this._token);
+            this._http_client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", Token);
         }
 
         public async Task<IEnumerable<PatientDto>> GetAllAsync()
@@ -68,34 +67,23 @@ namespace HMS.Shared.Proxies.Implementations
 
         public async Task<Patient> AddAsync(Patient patient)
         {
-            AddAuthorizationHeader();
-            PatientCreateDto patient_send = new PatientCreateDto
-            {
-                Email = patient.Email,
-                Password = patient.Password,
-                Name = patient.Name,
-                CNP = patient.CNP,
-                PhoneNumber = patient.PhoneNumber,
-                Role = patient.Role.ToString(),
-                BloodType = patient.BloodType.ToString(),
-                EmergencyContact = patient.EmergencyContact,
-                Allergies = patient.Allergies,
-                Weight = patient.Weight,
-                Height = patient.Height,
-                BirthDate = patient.BirthDate,
-                Address = patient.Address
-            };
-
             StringContent jsonContent = new StringContent(
-                JsonSerializer.Serialize(patient_send, new JsonSerializerOptions
+                JsonSerializer.Serialize(patient, new JsonSerializerOptions
                 {
                     PropertyNameCaseInsensitive = true,
-                    Converters = { new JsonStringEnumConverter(JsonNamingPolicy.CamelCase) } // without this, the enum values will not match
+                    Converters = { new JsonStringEnumConverter() }
                 }),
                 Encoding.UTF8,
                 "application/json");
 
             HttpResponseMessage response = await this._http_client.PostAsync(this.s_base_api_url + "patient", jsonContent);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                string error = await response.Content.ReadAsStringAsync();
+                Console.WriteLine($"Error: {response.StatusCode}, Body: {error}");
+            }
+
             response.EnsureSuccessStatusCode();
 
             string response_body = await response.Content.ReadAsStringAsync();
@@ -103,7 +91,7 @@ namespace HMS.Shared.Proxies.Implementations
             Patient patient_response = JsonSerializer.Deserialize<Patient>(response_body, new JsonSerializerOptions
             {
                 PropertyNameCaseInsensitive = true,
-                Converters = { new JsonStringEnumConverter(JsonNamingPolicy.CamelCase) } // without this, the enum values will not match
+                Converters = { new JsonStringEnumConverter() } // without this, the enum values will not match
 
             });
 
